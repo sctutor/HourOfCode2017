19b356d0f427ad570aec9c19907e9ae33894cf60
Mike Leonard <33782203+sctutor@users.noreply.github.com>
2017-12-04 10:06:56 -0500

    Update FanOnOffToggleNet.ino
    Updated comments, added change history, changed the polarity of state to be logical.

--- FanOnOffToggleNet.ino
+++ FanOnOffToggleNet.ino
@@ -1,4 +1,4 @@
-/***********************************************************
+/*******************************************************************************
  * Particle API Control of Mini-Fan
  * By Mike Leonard
  * By using a simple web form or If This Then That (IFTTT) we can remotely
@@ -6,15 +6,27 @@
  * To simplfy the code we have a TOGGLE function in addtion to ON and OFF functions
  * When you call the TOGGLE function the Mini-Fan goes to the opposite state (ON/OFF)
  * The Photon internal LED on pin D7 follows the (ON/OFF) state of the Mini-Fan for debug
- * *********************************************************/
 
+CHANGE HISTORY:
++-------------+----------+----------------------+-----------------------------------------+
+|    Date     | Version  |       Author         |                Comments                 |
+|-------------+----------+----------------------+-----------------------------------------+
+|  12/04/17   |   v1.0   |   Mike Leonard       |    Initial Release                      |
++-------------+----------+----------------------+-----------------------------------------+
+|             |          |                      |                                         |
++-------------+----------+----------------------+-----------------------------------------+
+|             |          |                      |                                         |
++-------------+----------+----------------------+-----------------------------------------+
+|             |          |                      |                                         |
++-------------+----------+----------------------+-----------------------------------------+
+*******************************************************************************/
 
 //Assign variable names to Photon pin numbers
 int FanOut = D2;
 int IntLedOut = D7;
 
 //Define and initialize the variable state which will keep track of if the Mini-Fan is ON or OFF 
-int state = 1;
+int state = 0;
 
 //The setup() function runs only one time
 //It sets the mode of the Photon pins D2 and D7 to OUTPUT
@@ -36,7 +48,7 @@ void loop()
 {
 }
 
-//The Local Function accepts 3 different command parameters (on, off, toggle)
+//The Local Function FanControl() accepts 3 different parameters (on, off, toggle)
 //A value of -1 is returned if and invalid parameter is passed
 //Enhancements could be made to add SPEED and DIRECTION parameters as well
 int FanControl(String command) {
@@ -44,18 +56,18 @@ int FanControl(String command) {
     if (command =="on") {
         digitalWrite(FanOut,HIGH);
         digitalWrite(IntLedOut,HIGH);
-        state = 0;
+        state = 1;
         return 1;
     }
     else if (command =="off") {
         digitalWrite(FanOut,LOW);
         digitalWrite(IntLedOut,LOW);
-        state = 1;
+        state = 0;
         return 0;
     }
     else if (command =="toggle") {
-        digitalWrite(FanOut, (state) ? HIGH : LOW);
-        digitalWrite(IntLedOut, (state) ? HIGH : LOW);
+        digitalWrite(FanOut, (state) ? LOW : HIGH);
+        digitalWrite(IntLedOut, (state) ? LOW : HIGH);
         state = !state;
         return 3;
     }
